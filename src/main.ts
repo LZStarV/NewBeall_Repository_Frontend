c6e34048ef9f4869609e3db44df431d9015ea823
IT-GGbond <2564183149@qq.com>
2025-05-14 22:08:00 +0800

    新增主题样式配置文件

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/routers/index.ts';
 import '@layui/layui-vue/lib/index.css';
-
+// 引入全局样式
+import '@/styles/index.scss';
 
 const pinia = createPinia()
 
