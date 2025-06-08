7ba962b3394d9c25397fc25ba9487ae313c0b24e
LZStarV <3654498270@qq.com>
2025-06-08 23:58:16 +0800

    test文件配置

---
+++ tests/index.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from 'vitest';
+import Home from '@/pages/HomePage.vue';
+
+test('first test', () => {
+    expect(1 + 1).toBe(2);
+});
+
+test('vuee test', () => {
+    console.log(Home);
+    
+});
