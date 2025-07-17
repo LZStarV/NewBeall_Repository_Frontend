8dde92293c00dea11693c1d799d3d2f7004f573a
LZStarV <154000207+LZStarV@users.noreply.github.com>
2025-07-17 16:03:52 +0800

    fix(typescript): 将 store 目录重命名为 stores 并更新导入 (#14)
    为了保持一致性，将 'store' 目录重命名为 'stores'。更新了所有相关的导入路径和 Vite 别名配置。添加了一个 Husky commit-msg 钩子，用于提交消息 linting。

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,13 +7,12 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue';
-import { useUserStore } from '@/store/modules/user';
+import { useUserStore } from '@/stores/modules/user';
 // import type { UserType } from '@/types/user';
 
 const userStore = useUserStore();
 
 const userInfo = ref(userStore.userData);
-
 </script>
 
 <style lang="scss" scoped></style>
