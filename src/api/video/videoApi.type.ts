6f356d06a4f7e45ab1e16de8a823417c0a4301df
Aurora-N <Aurorallz@outlook.com>
2025-07-24 15:09:18 +0800

    feat(pages): 初步完成视频页面

---
+++ src/api/video/videoApi.type.ts
@@ -0,0 +1,16 @@
+// 视频数据
+export interface VideoData {
+  active: string;
+  child: VideoData[];
+  icon: string;
+  id: number;
+  indexSortnum: number;
+  isDisplay: string;
+  isIndex: string;
+  name: string;
+  pid: number;
+  sortnum: number;
+  url: string;
+  videoDesc: string;
+}
+
