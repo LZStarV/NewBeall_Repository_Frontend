696f6922d8538bfb94381a71c99e4579b329f240
Aurora-N <63760450+Aurora-N@users.noreply.github.com>
2025-08-03 22:00:16 +0800

    feat(pages): 初略完成新建设计报价管理页面/修改部分样式代码位置

---
+++ src/api/client/clinetApi.type.ts
@@ -0,0 +1,69 @@
+// 客户类型
+export interface ClientType {
+  address: string;
+  assignUser: number;
+  clientArea: string;
+  clientCategory: number;
+  clientCity: string;
+  clientProvince: string;
+  clientSize: number;
+  clientSizeMsg: string;
+  clientSource: string;
+  clientStatus: string;
+  clientStreet: string;
+  clientWebsite: string;
+  contactUser: string;
+  contacts: string;
+  createtime: string;
+  createuser: number;
+  delflag: number;
+  email: string;
+  fax: string;
+  id: number;
+  job: string;
+  modifytime: string;
+  note: string;
+  pinyin: string;
+  qq: string;
+  remark: string;
+  revisitTime: string;
+  tel: string;
+  wechat: string;
+}
+
+// 查询客户列表类型
+export interface ClientQueryListType {
+  // 客户类型（可为空，查询时使用）
+  categoryName?: string;
+  // 客户来源(可为空，查询时使用)
+  clientSource?: string;
+  // 跟进状态(可为空，查询时使用)
+  clientStatus?: string;
+  // 联系人名称(可为空，查询时使用)
+  contactUser?: string;
+  // 公司名称(可为空，查询时使用)
+  contacts?: string;
+  // 每页数量
+  limit: string;
+  // 目前浏览的总数
+  offset: number;
+  // 排序（可为空，asc或desc）
+  order?: string;
+  // 拼音查询
+  pinyin?: string;
+  // 排序类型，根据创建时间排序或者拼音（可为空，revisitTime或pinyin）
+  sort?: string;
+}
+
+// 用户树类型
+export interface UserTreeType {
+  checked: boolean;
+  chkDisabled: boolean;
+  id: number;
+  isOpen: boolean;
+  isUser: string;
+  name: string;
+  nocheck: boolean;
+  open: boolean;
+  pId: number;
+}
